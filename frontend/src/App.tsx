1ffe471a7bba505cdeb7a8737a07be121d883134
tojix-code <tojix46@gmail.com>
2026-02-28 19:26:33 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,30 @@
+import Navbar  from "./components/Navbar";
+import ProductCard from "./components/ProductCard";
+import Footer from "./components/Footer";
+
 export default function App() {
-  return (
-    <div className="bg-blue-500 text-white p-10">
-      Tailwind is working 🚀
-    </div>
-  );
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <Navbar />
+
+            <div className="p-6 grid gap-6 md:grid-cols-3">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard /> 
+
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />     
+
+                
+
+            </div>
+
+            <Footer />
+        </div>
+    );
 }
