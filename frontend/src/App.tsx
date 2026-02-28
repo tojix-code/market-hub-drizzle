de17bfff9819344bbcc4ceb71f6ef13cf59eb942
tojix-code <tojix46@gmail.com>
2026-02-28 18:39:33 +0530

    update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>
+                Hello world! This is the frontend of our market hub application.
+            </h1>
+        </div>
+    )
+}
+
+export default App; 
