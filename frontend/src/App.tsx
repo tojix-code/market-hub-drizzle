a1f427a638d8206354d0fe67447b5e059686c1b4
tojix-code <tojix46@gmail.com>
2026-03-01 12:23:00 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import Navbar  from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 export default function App() {
     return (
         <div className="bg-gray-100 min-h-screen">
             <Navbar />
+
+
+            <main className="flex-grow">
+                <Hero />
+            
 
             <div className="p-6 grid gap-6 md:grid-cols-3">
                 <ProductCard />
@@ -23,8 +29,10 @@ export default function App() {
                 
 
             </div>
+            </main>
 
             <Footer />
         </div>
+        
     );
 }
