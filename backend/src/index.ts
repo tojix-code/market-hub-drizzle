8d01a28148d67b06aa8320a7685f8625c951c2a5
tojix-code <tojix46@gmail.com>
2026-03-02 19:50:52 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,16 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 // Routes
-import userRoute from "./routes/users";
-import storeRoute from "./routes/stores";
-import categoryRoute from "./routes/categories";
-import productRoute from "./routes/products";
-import orderRoute from "./routes/orders";
-import paymentRoute from "./routes/payments";
-import wishlistRoute from "./routes/wishlist";
-import ratingRoute from "./routes/ratings";
-import couponRoute from "./routes/coupons";
-import addressRoute from "./routes/address";
+import userRoute from "./routes/users.js";
+import storeRoute from "./routes/stores.js";
+import categoryRoute from "./routes/categories.js";
+import productRoute from "./routes/products.js";
+import orderRoute from "./routes/orders.js";
+import paymentRoute from "./routes/payments.js";
+import wishlistRoute from "./routes/wishlist.js";
+import ratingRoute from "./routes/ratings.js";
+import couponRoute from "./routes/coupons.js";
+import addressRoute from "./routes/address.js";
 
 const app = new Hono();
 
