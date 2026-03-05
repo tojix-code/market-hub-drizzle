d1ad9b495c14f0ad382ccf7f319d86f16fa2ce30
tojix-code <tojix46@gmail.com>
2026-03-05 18:42:35 +0530

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,47 @@
-import Navbar  from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
-export default function App() {
-    return (
-        <div className="bg-gray-100 min-h-screen">
-            <Navbar />
+import AdminLayout from "./components/admin/AdminLayout";
+import StoreLayout from "./components/store/StoreLayout";
 
+function HomePage() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
 
-            <main className="flex-grow">
-                <Hero />
-            
+      <main className="flex-grow">
+        <Hero />
 
-            <div className="p-6 grid gap-6 md:grid-cols-3">
-                <ProductCard />
-                <ProductCard />
-                <ProductCard /> 
+        <div className="p-6 grid gap-6 md:grid-cols-3">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
 
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
 
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />     
-
-                
-
-            </div>
-            </main>
-
-            <Footer />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </div>
-        
-    );
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<AdminLayout />} />
+      <Route path="/store" element={<StoreLayout />} />
+    </Routes>
+  );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
